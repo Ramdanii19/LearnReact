6649833f5e5bec2114a06659637053891bc6551a
Ramdani <110803481+Ramdanii19@users.noreply.github.com>
2024-10-26 08:28:13 +0700

    Rendereing List

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,15 +1,32 @@
 import CardProduct from "../components/Fragments/CardProduct"
 
+const products = [
+  {
+    name: "Sepatu Baru",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, delectus enim. Architecto eveniet distinctio totam illo minima adipisci, autem vel sed odio expedita sequi a rem. Dicta deleniti vitae minima.",
+    price: "Rp.1.000.000",
+    img: "/images/sepatu.png"
+  },
+  {
+    name: "Sepatu Baru2",
+    description: " distinctio totam illo minima adipisci, autem vel sed odio expedita sequi a rem. Dicta deleniti vitae minima.",
+    price: "Rp.2.000.000",
+    img: "/images/sepatu.png"
+  }
+]
+
 const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header img="/images/sepatu.png"></CardProduct.Header>
-        <CardProduct.Body title="Sepatu Baru">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, delectus enim. Architecto eveniet distinctio totam illo minima adipisci, autem vel sed odio expedita sequi a rem. Dicta deleniti vitae minima.
-        </CardProduct.Body>
-        <CardProduct.Footer price="Rp.1.000.000"></CardProduct.Footer>
-      </CardProduct>
+      {products.map((product) => (
+        <CardProduct>
+          <CardProduct.Header img={product.img}></CardProduct.Header>
+          <CardProduct.Body name={product.name}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price}></CardProduct.Footer>
+        </CardProduct>
+      ))}
     </div>
   )
 }
