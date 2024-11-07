bbb080029b6fbd2738e2551eb567a003bd8672d4
Ramdani <110803481+Ramdanii19@users.noreply.github.com>
2024-11-07 10:54:02 +0700

    Implementasi Redux

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,20 +1,21 @@
-import { useEffect, useRef, useState } from "react";
-import Button from "../components/Elements/Button";
+import { useEffect, useState } from "react";
 import CardProduct from "../components/Fragments/CardProduct"
 import { getProducts } from "../services/product.service";
 import { useLogin } from "../hooks/useLogin";
+import TableCart from "../components/Fragments/TableCart";
+import Navbar from "../components/Layouts/Navbar";
 
 
 
 const ProductsPage = () => {
-  const [cart, setCart] = useState([]);
-  const [totalPrice, setTotalPrice] = useState(0);
+  // const [cart, setCart] = useState([]);
+  // const [totalPrice, setTotalPrice] = useState(0);
   const [products, setProducts] = useState([]);
-  const username = useLogin();
+  useLogin();
 
-  useEffect(() => {
-    setCart(JSON.parse(localStorage.getItem("cart")) || []);
-  }, []);
+  // useEffect(() => {
+  //   setCart(JSON.parse(localStorage.getItem("cart")) || []);
+  // }, []);
 
   useEffect(() => {
     getProducts((data) => {
@@ -22,57 +23,54 @@ const ProductsPage = () => {
     });
   }, []);
 
-  useEffect(() => {
-    if (products.length > 0 && cart.length > 0) {
-      const sum = cart.reduce((acc, item) => {
-        const product = products.find((product) => product.id === item.id);
-        return acc + product.price * item.qty;
-      }, 0);
-      setTotalPrice(sum);
-      localStorage.setItem("cart", JSON.stringify(cart));
-    }
-  }, [cart, products])
+  // useEffect(() => {
+  //   if (products.length > 0 && cart.length > 0) {
+  //     const sum = cart.reduce((acc, item) => {
+  //       const product = products.find((product) => product.id === item.id);
+  //       return acc + product.price * item.qty;
+  //     }, 0);
+  //     setTotalPrice(sum);
+  //     localStorage.setItem("cart", JSON.stringify(cart));
+  //   }
+  // }, [cart, products])
 
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    window.location.href = "/login"
-  }
+  // const handleLogout = () => {
+  //   localStorage.removeItem("token");
+  //   window.location.href = "/login"
+  // }
 
-  const handleAddToCart = (id) => {
-    if (cart.find((item) => item.id === id)) {
-      setCart(
-        cart.map((item) =>
-          item.id === id ? { ...item, qty: item.qty + 1 } : item
-        )
-      );
-    } else {
-      setCart([...cart, { id: id, qty: 1 }]);
-    }
-  };
+  // const handleAddToCart = (id) => {
+  //   if (cart.find((item) => item.id === id)) {
+  //     setCart(
+  //       cart.map((item) =>
+  //         item.id === id ? { ...item, qty: item.qty + 1 } : item
+  //       )
+  //     );
+  //   } else {
+  //     setCart([...cart, { id: id, qty: 1 }]);
+  //   }
+  // };
 
   //useRef
-  const cartRef = useRef(JSON.parse(localStorage.getItem("cart")) || []);
-  const handleAddToCartRef = (id) => {
-    cartRef.current = [...cartRef.current, { id: id, qty: 1 }];
-    localStorage.setItem("cart", JSON.stringify(cartRef.current));
-  };
+  // const cartRef = useRef(JSON.parse(localStorage.getItem("cart")) || []);
+  // const handleAddToCartRef = (id) => {
+  //   cartRef.current = [...cartRef.current, { id: id, qty: 1 }];
+  //   localStorage.setItem("cart", JSON.stringify(cartRef.current));
+  // };
 
-  const totalPriceRef = useRef(null);
+  // const totalPriceRef = useRef(null);
 
-  useEffect(() => {
-    if (cart.length > 0) {
-      totalPriceRef.current.style.display = "table-row";
-    } else {
-      totalPriceRef.current.style.display = "none";
-    }
-  }, [cart]);
+  // useEffect(() => {
+  //   if (cart.length > 0) {
+  //     totalPriceRef.current.style.display = "table-row";
+  //   } else {
+  //     totalPriceRef.current.style.display = "none";
+  //   }
+  // }, [cart]);
 
   return (
     <div className="">
-      <div className="flex justify-end h-20 bg-blue-600 text-white items-center px-10">
-        {username}
-        <Button className="ml-5 bg-black" onClick={handleLogout}>Logout</Button>
-      </div>
+      <Navbar />
       <div className="flex justify-center py-5">
         <div className="w-4/6 flex flex-wrap">
           {products.length > 0 && products.map((product) => (
@@ -81,12 +79,13 @@ const ProductsPage = () => {
               <CardProduct.Body name={product.title}>
                 {product.description}
               </CardProduct.Body>
-              <CardProduct.Footer price={product.price} id={product.id} handleAddToCart={handleAddToCart}></CardProduct.Footer>
+              {/* <CardProduct.Footer price={product.price} id={product.id} handleAddToCart={handleAddToCart}></CardProduct.Footer> */}
+              <CardProduct.Footer price={product.price} id={product.id}></CardProduct.Footer>
             </CardProduct>
           ))}</div>
         <div className="w-2/6">
           <h1 className="text-3xl font-bold text-blue-600 ml-5 mb-2">Cart</h1>
-          <table className="text-left table-auto border-separate border-spacing-x-5">
+          {/* <table className="text-left table-auto border-separate border-spacing-x-5">
             <thead>
               <tr>
                 <th>Product</th>
@@ -112,7 +111,8 @@ const ProductsPage = () => {
                 <td><b>$ {totalPrice.toLocaleString('id-ID', { styles: 'currency', currency: 'USD' })}</b></td>
               </tr>
             </tbody>
-          </table>
+          </table> */}
+          <TableCart products={products} />
         </div>
       </div>
     </div >
